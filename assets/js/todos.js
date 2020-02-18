7c4c7efb20253b8b3046263f52101d765d29524c
RedBalloon24 <worldtraveler757@gmail.com>
2020-02-18 17:25:00 +0100

    fourth commit

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -16,6 +16,6 @@ $("input[type='text']").keypress(function(e){
     if(e.which === 13){
         var todoText = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+        $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>");
     }
 });
