dde3e767830ec18ecb7ccfce750fd7aab8f5f60e
RedBalloon24 <worldtraveler757@gmail.com>
2020-02-18 16:13:05 +0100

    third commit

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,21 @@
-//Check Off Specific Todos By Clicking
-$("li").click(function(){
- $(this).toggleClass("completed");
+//Check Off Specific Todos
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+//Click on X to delete todos
+$("ul").on("click", "span", function(e){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
+
+//Type and create todos
+$("input[type='text']").keypress(function(e){
+    if(e.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
